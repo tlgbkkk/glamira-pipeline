91659757307c29f6773f4409cd74e140a9729ff1
tlgb <tlgbkakaka@gmail.com>
2026-05-13 13:45:52 +0700

    truyen bucket name vao sh

--- cloud_function/deploy.sh
+++ cloud_function/deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# get project id
 PROJECT_ID=$(gcloud config get-value project)
+
+CONFIG_PATH="../src/config.py"
+
+if [ -f "$CONFIG_PATH" ]; then
+    BUCKET_NAME=$(grep "BUCKET_NAME" $CONFIG_PATH | sed -E 's/.*"([^"]+)".*/\1/')
+else
+    echo "Not found $CONFIG_PATH!"
+    exit 1
+fi
 
 echo "Deploying Cloud Function on Project: $PROJECT_ID..."
 
 gcloud functions deploy gcs_to_bq_trigger \
 --runtime python310 \
---trigger-resource gcs-project-raw-data \
+--trigger-resource $BUCKET_NAME \
 --trigger-event google.storage.object.finalize \
 --entry-point trigger_bigquery_load \
 --set-env-vars GCP_PROJECT=$PROJECT_ID \
